02def1710164eaae9d1ddf21c0c2ea1e6fd7dbdf
allthesignals <wilburnforce@gmail.com>
2021-02-12 13:35:42 -0500

    Permit known origins only

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,11 +1,17 @@
 const request = require('superagent');
 const { URL } = require('url');
 
+const ALLOWED_ORIGINS = [
+  'https://nycplanning.carto.com',
+  'https://nycplanning-web.carto.com',
+  'https://dcpbuilder.carto.com',
+];
+
 exports.handler = async (event) => {
   const site = event.queryStringParameters && event.queryStringParameters.site
   const { origin } = new URL(site);
 
-  if (origin !== 'https://nycplanning.carto.com' && origin !== 'https://nycplanning-web.carto.com') {
+  if (!ALLOWED_ORIGINS.includes(origin)) {
     return {
       statusCode: 500,
       body: 'Origin not allowed.',
